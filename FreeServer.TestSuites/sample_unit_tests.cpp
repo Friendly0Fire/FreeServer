2e3c463471be2a43dece138ba2a284fde672af23
Bobby David <dartkietanmartaru@gmail.com>
2020-12-08 21:48:47 -0500

    Add test stubs.

---
+++ FreeServer.TestSuites/sample_unit_tests.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+namespace FreeServer_TestSuites_UnitTests {
+	TEST(SampleTestCaseName, SampleTestName) {
+		EXPECT_EQ(1, 1);
+		EXPECT_TRUE(true);
+	}
+	TEST(SampleTestCaseName, SampleTestName2) {
+		EXPECT_EQ(2, 2);
+		EXPECT_TRUE(true);
+	}
+}
