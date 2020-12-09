c47bf2ff34c70e1bfd78e810846b611967e55b70
Bobby David <dartkietanmartaru@gmail.com>
2020-12-08 22:16:10 -0500

    add main method.

--- FreeServer.TestSuites/sample_unit_tests.cpp
+++ FreeServer.TestSuites/sample_unit_tests.cpp
@@ -8,4 +8,9 @@ namespace FreeServer_TestSuites_UnitTests {
 		EXPECT_EQ(2, 2);
 		EXPECT_TRUE(true);
 	}
+
+	int main(int argc, char** argv) {
+		::testing::InitGoogleTest(&argc, argv);
+		return RUN_ALL_TESTS();
+	}
 }
